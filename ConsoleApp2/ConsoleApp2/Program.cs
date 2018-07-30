eb9aac8facd6daf2b57388acdb7b037134ace309
mag12242002 <41813467+mag12242002@users.noreply.github.com>
2018-07-30 13:55:55 +0800

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("333");
+            Console.WriteLine("444");
+            Console.WriteLine("555");
+            Console.WriteLine("666");
+        }
+    }
+}
