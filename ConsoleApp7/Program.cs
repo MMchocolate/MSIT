143a537563c2683a1c73a170b37c9cdfd8f947af
debbie <deborah7229@gmail.com>
2018-07-30 13:42:38 +0800

    1

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int j=10;
+            double k;
+            k = j;
+            k = 0.5;
+            double g;
+            g=k;
+           
+        }
+    }
+}
